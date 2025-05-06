1982533038f2920da4f58e97f8d71edecda703e2
Roman Kychkin <roman.heilong@gmail.com>
2025-05-06 09:04:21 +0800

    Edit title

--- next/src/app/(root)/(static)/tour/yakutsk/page.tsx
+++ next/src/app/(root)/(static)/tour/yakutsk/page.tsx
@@ -8,7 +8,7 @@ import {
 import TourPage from "@/components/tour/TourPage"
 
 export const metadata = {
-  title: "Харбин для детей: наука, природа и приключения",
+  title: "Харбин для группы из Якутска",
   description:
     "Увлекательное путешествие, где каждый день наполнен новыми открытиями для детей и родителей",
 }
